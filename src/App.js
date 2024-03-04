f63ed264e622418c9e60a3912da10a43de326984
Olykir <turtlederp806@icloud.com>
2024-03-04 01:25:55 -0600

    typewriter not really lol

--- src/App.js
+++ src/App.js
@@ -5,17 +5,14 @@ import {
   faSteam,
   faTiktok,
   faReact,
-  faNode,
   faInternetExplorer,
   faGithub,
   faNodeJs,
   faFigma,
   faApple,
-  faAndroid,
-  faStripe,
 } from "@fortawesome/free-brands-svg-icons";
 import "./App.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Projects = [
   {
@@ -46,35 +43,80 @@ function RenderLinkIcon({ link }) {
 function ProjectCard(project) {
   return (
     <div className="CardContainer">
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          flexDirection: "row",
-        }}
-      >
-        <div className="ProjectInfoContainer">
-          <h3 className="ProjectName">{project.title}</h3>
-          <p className="ProjectDescription">{project.description}</p>
+      <div className="CardContent">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            flexDirection: "row",
+          }}
+        >
+          <div className="ProjectInfoContainer">
+            <h3 className="ProjectName">{project.title}</h3>
+            <p className="ProjectDescription">{project.description}</p>
+          </div>
+          {project.link ? (
+            <a
+              className="IconLink IconLinkHover"
+              style={{ fontSize: "1.5rem" }}
+              href={project.link}
+            >
+              <RenderLinkIcon link={project.link} />
+            </a>
+          ) : (
+            void 0
+          )}
         </div>
-        {project.link ? (
-          <a
-            className="IconLink IconLinkHover"
-            style={{ fontSize: "1.5rem" }}
-            href={project.link}
-          >
-            <RenderLinkIcon link={project.link} />
-          </a>
-        ) : (
-          void 0
-        )}
-      </div>
-      <div className="ProjectTechUsedContainer">
-        {project.tech.map((ico) => (
-          <FontAwesomeIcon className="TechIcon" icon={ico} />
-        ))}
+        <div className="ProjectTechUsedContainer">
+          {project.tech.map((ico) => (
+            <FontAwesomeIcon className="TechIcon" icon={ico} />
+          ))}
+        </div>
       </div>
     </div>
+  );
+}
+
+function TypewriterEffectWithMultipleLines({ lines }) {
+  const [currentLine, setCurrentLine] = useState(lines[0]);
+
+  const [currentText, setCurrentText] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (currentText.length < currentLine.length) {
+      const timeout = setTimeout(() => {
+        setCurrentText((prev) => prev + currentLine[currentIndex]);
+        setCurrentIndex((prev) => prev + 1);
+      }, 175);
+
+      return () => {
+        clearTimeout(timeout);
+      };
+    } else {
+      const timeout = setTimeout(() => {
+        if (lines.indexOf(currentLine) > lines.length) {
+          setCurrentLine("test");
+        } else {
+          const currentIndex = lines.indexOf(currentLine);
+          setCurrentLine(lines[currentIndex + 1]);
+        }
+
+        setCurrentText("");
+        setCurrentIndex(0);
+      }, 1500);
+
+      return () => {
+        clearTimeout(timeout);
+      };
+    }
+  }, [currentIndex, currentLine, currentIndex]);
+
+  return (
+    <span>
+      {currentText}
+      <span className="TypewriterBar">_</span>
+    </span>
   );
 }
 
@@ -86,7 +128,11 @@ function App() {
           <div>
             <div className="InformationBoutSelf">
               {" "}
-              <h2 className="BigFatTypewriterName">Olykir</h2>{" "}
+              <h2 className="BigFatTypewriterName">
+                <TypewriterEffectWithMultipleLines
+                  lines={["Olykir", "Landen"]}
+                />
+              </h2>{" "}
               {/* maybe do cool typewriter effect with bunch of different aliases */}
               <p className="LittleSmallUndernameTitle">
                 Awesome Sauce Spaghetti Code
